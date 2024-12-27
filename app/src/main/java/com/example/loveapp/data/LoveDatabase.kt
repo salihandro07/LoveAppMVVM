0a801b2cbee05c2272e876f20419285457e2a2d5
salihandro07 <khamrayumovsalih2gmail.com>
2024-12-27 23:28:42 +0600

    LoveApp78

--- app/src/main/java/com/example/loveapp/data/LoveDatabase.kt
+++
@@ -1,8 +0,0 @@
-package com.example.loveapp.data
-
-import androidx.room.RoomDatabase
-import com.example.loveapp.data.local.ResultDao
-
-abstract class LoveDatabase: RoomDatabase() {
-    abstract fun loveDao(): ResultDao
-}
